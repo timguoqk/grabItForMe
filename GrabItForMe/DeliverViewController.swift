365fa3e2956b504115d8944c66e92bbee4220dcf
Cary <cary.hejiang@gmail.com>
2015-04-04 22:34:11 -0700

    Map annotation place class

--- GrabItForMe/DeliverViewController.swift
+++ GrabItForMe/DeliverViewController.swift
@@ -34,6 +34,57 @@ class DeliverViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         mapView.setRegion(MKCoordinateRegionMakeWithDistance(uclaCoord, 1000, 1000), animated: true)
         var option = UIBarButtonItem(title: "Options", style: UIBarButtonItemStyle.Plain, target: self, action: "gotoProfile")
         navigationItem.leftBarButtonItem = option
+        
+        // show places on map
+        let DiddyRiese = Place(title: "Diddy Riese",
+            locationName: "926 Broxton Ave",
+            discipline: "Dessert",
+            coordinate: CLLocationCoordinate2D(latitude: 34.063039, longitude: -118.446819))
+        
+        mapView.addAnnotation(DiddyRiese)
+        
+        let Degrees = Place(title: "800 Degrees Neapolitan Pizzeria",
+            locationName: "10889 Lindbrook Dr",
+            discipline: "Pizza",
+            coordinate: CLLocationCoordinate2D(latitude: 34.0598619, longitude: -118.444444))
+        
+        mapView.addAnnotation(Degrees)
+        
+        let InNOut = Place(title: "In-N-Out Burger",
+            locationName: "922 Gayley Ave",
+            discipline: "Fast Food",
+            coordinate: CLLocationCoordinate2D(latitude: 34.063077, longitude: -118.448031))
+        
+        mapView.addAnnotation(InNOut)
+        
+        let Gushi = Place(title: "Gushi",
+            locationName: "978 Gayley Ave",
+            discipline: "Korean",
+            coordinate: CLLocationCoordinate2D(latitude: 34.062301, longitude: -118.447871))
+        
+        mapView.addAnnotation(Gushi)
+        
+        let WholeFoods = Place(title: "Whole Foods Market",
+            locationName: "1050 Gayley Ave",
+            discipline: "Supermarket",
+            coordinate: CLLocationCoordinate2D(latitude: 34.061217, longitude: -118.446906))
+        
+        mapView.addAnnotation(WholeFoods)
+        
+        let Ralphs = Place(title: "Ralphs",
+            locationName: "10861 Le Conte",
+            discipline: "Supermarket",
+            coordinate: CLLocationCoordinate2D(latitude: 34.063299, longitude: -118.443998))
+        
+        mapView.addAnnotation(Ralphs)
+        
+        let Trader = Place(title: "Trader Joe's",
+            locationName: "1000 Glendon Ave",
+            discipline: "Supermarket",
+            coordinate: CLLocationCoordinate2D(latitude: 34.062539, longitude: -118.443982))
+        
+        mapView.addAnnotation(Trader)
+
     }
 
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
